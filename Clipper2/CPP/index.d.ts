848bed3f9f117ddb5ad16938879c91d8409b967a
Venelin Efremov <ghent360@iqury.us>
2017-11-20 00:23:18 -0800

    Add documentation to the typescript interface. Remove the prefix from the enum values.

--- Clipper2/CPP/index.d.ts
+++ Clipper2/CPP/index.d.ts
@@ -1,30 +1,79 @@
+/*******************************************************************************
+* Author    :  Venelin Efremov                                                 *
+* Version   :  10.0 (beta)                                                     *
+* Date      :  8 Noveber 2017                                                  *
+* Website   :  http://www.angusj.com                                           *
+* Copyright :  Venelin Efremov 2017                                            *
+* Purpose   :  Javascript interface for base clipping module                   *
+* License   : http://www.boost.org/LICENSE_1_0.txt                             *
+*******************************************************************************/
+
+/*
+    Note: when making changes to clipper_js.cpp these have to be reflected here as well.
+
+    Javascript interface definitions:
+
+      - Point: any type with properties x and y of type number;
+
+      - Path: a collection of points describing the polygon. These can be open 
+              or closed.
+
+      - Paths: a collection of polygons describing a shape. Shapes have a fill-rule.
+               The fill-rule describes which parts of the shape are to be colored.
+               For examples see: https://en.wikipedia.org/wiki/Nonzero-rule
+               or https://en.wikipedia.org/wiki/Even%E2%80%93odd_rule
+
+      - PathType: when adding polygons to the Clipper you have to tell which set the
+                  polygon belongs to. The final clipping operation is performed on the
+                  polygon sets. For example if you have shape A and want to cut shape B
+                  from it, you would put all polygons from shape A as ptSubject and all
+                  polygons from shape B as ptClip.
+
+                  Consider the clipping operation as something like:
+                  ptSubject (op) ptClip
+
+                  Where (op) is the operation code from ClipType.
+
+      - PoinType: the last parameter of the execute method is a type of the objects we
+                  should construct when computing the solution. The provided type has to
+                  implement a constructor which takes two numbers (x and y coordinate).
+                  If the PointType is not specified, we would create  tuples {x:number, y:number}.
+
+      - Precision: the Clipper library operates internaly in integer coordinates. To convert
+                   from JavaScript numbers, which are floating point, we provide a 
+                   precisionMultiplier in the constructor. This means all input coordinates
+                   would be multiplied by that number and all output coordinates would be
+                   divided by that number when converted to floating point.
+ */
 export interface Point {
     readonly x: number;
     readonly y: number;
 }
 
+// These are C++ enums, but they appear as classes in JavaScriot
 export declare class ClipType {
-    static ctNone:ClipType;
-    static ctIntersection:ClipType;
-    static ctUnion:ClipType;
-    static ctDifference:ClipType;
-    static ctXor:ClipType;
+    static None:ClipType;
+    static Intersection:ClipType;
+    static Union:ClipType;
+    static Difference:ClipType;
+    static Xor:ClipType;
 }
 
 export declare class PathType {
-    static ptClip:PathType;
-    static ptSubject:PathType;
+    static Clip:PathType;
+    static Subject:PathType;
 }
 
 export declare class FillRule {
-    static frEvenOdd:FillRule;
-    static frNonZero:FillRule;
-    static frPositive:FillRule;
-    static frNegative:FillRule;
+    static EvenOdd:FillRule;
+    static NonZero:FillRule;
+    static Positive:FillRule;
+    static Negative:FillRule;
 }
 
 export declare interface Result<T> {
     success:boolean;
+    // The following two properties are defined if success is true.
     solution_closed?:Array<Array<T>>;
     solution_open?:Array<Array<T>>;
 }
@@ -32,12 +81,12 @@ export declare interface Result<T> {
 export declare class Clipper<T> {
     constructor(precisionMultiplier:number);
 
-    addPath(path:Array<Point>, pathtype:PathType, is_open:boolean):void;
-    addPaths(paths:Array<Array<Point>>, pathtype:PathType, is_open:boolean):void;
+    addPath(polygon:Array<Point>, pathType:PathType, isOpen:boolean):void;
+    addPaths(shape:Array<Array<Point>>, pathType:PathType, isOpen:boolean):void;
     execute(
         clipType:ClipType,
         fillRule:FillRule,
-        pointType:new(x:number, y:number) => T):Result<T>;
+        pointType?:new(x:number, y:number) => T):Result<T>;
     
     clear():void;
     delete():void;
