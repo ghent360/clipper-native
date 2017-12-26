67f9295e0a86c2072173ca727a924152eb2356b8
Venelin Efremov <ghent360@iqury.us>
2017-12-26 02:29:49 -0800

    Test AddPaths.

--- Clipper2/nodejs/hello.js
+++ Clipper2/nodejs/hello.js
@@ -6,3 +6,6 @@ c.addPath([1, 100], "subject");
 c.addPath([{x:1, y:100}], "clip");
 c.addPath(Float64Array.of(1, 100), "subject");
 c.addPath(Int32Array.of(1, 100), "invalid", (e) => {console.log(`e is ${e}`)});
+
+c.addPaths([Int32Array.of(1, 100), Float64Array.of(1, 100), [55, 77, 33, 11]], "clip");
+console.log('complete');
