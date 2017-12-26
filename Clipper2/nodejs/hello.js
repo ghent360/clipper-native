1bc006104e49236a8c1f09fa76ca82d389140141
Venelin Efremov <ghent360@iqury.us>
2017-12-26 02:22:00 -0800

    Test error callback.

--- Clipper2/nodejs/hello.js
+++ Clipper2/nodejs/hello.js
@@ -5,3 +5,4 @@ console.log(`Precision ${c.precision()}`);
 c.addPath([1, 100], "subject");
 c.addPath([{x:1, y:100}], "clip");
 c.addPath(Float64Array.of(1, 100), "subject");
+c.addPath(Int32Array.of(1, 100), "invalid", (e) => {console.log(`e is ${e}`)});
